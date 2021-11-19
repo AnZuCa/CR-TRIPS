8349fa871be2ad54d3a858dec1c1d875573a0d31
Marvin031 <marvinch31@gmail.com>
2021-11-19 17:36:29 -0600

    prueba

--- src/java/logica/modelo/Conexion.java
+++ src/java/logica/modelo/Conexion.java
@@ -14,17 +14,28 @@ import java.sql.SQLException;
  * @author hp
  */
 public class Conexion {
+<<<<<<< HEAD
    private String USERNAME="root";
    private String PASSWORD="root";
     //private String USERNAME="globales@mysqlglobales";
     //private String PASSWORD="Lala1234";
+=======
+//    private String USERNAME="root";
+//    private String PASSWORD="root";
+    private String USERNAME="globales@mysqlglobales";
+    private String PASSWORD="Lala1234";
+>>>>>>> parent of 2f66e5a (Cambios)
     private String HOST="localhost";
     private String PORT="3306";
     private String DATABASE="cr_trips";
     private String CLASSNAME="com.mysql.cj.jdbc.Driver";
     //private String URL="jdbc:mysql://"+HOST+":"+PORT+"/"+DATABASE+"?useSSL=false";
+<<<<<<< HEAD
     private String URL="jdbc:mysql://"+HOST+":"+PORT+"/"+DATABASE+"?allowPublicKeyRetrieval=true&useSSL=false";
     //private String URL="jdbc:mysql://mysqlglobales.mysql.database.azure.com:3306/cr_trips?useSSL=false&requireSSL=false";
+=======
+    private String URL="jdbc:mysql://mysqlglobales.mysql.database.azure.com:3306/cr_trips?useSSL=false&requireSSL=false";
+>>>>>>> parent of 2f66e5a (Cambios)
     private Connection con;
     
     public Conexion() {
